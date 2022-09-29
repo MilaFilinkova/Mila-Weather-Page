9fddd0591db3bce2e38eafef43282b0912085246
Mila Filinkova <109034507+MilaFilinkova@users.noreply.github.com>
2022-09-29 19:51:55 +0200

    Time is updated

--- src/app.js
+++ src/app.js
@@ -1,3 +1,28 @@
+function formatDate(date) {
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let dayIndex = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[dayIndex];
+
+  return `${day} ${hours}:${minutes}`;
+}
+
 function showTemperature(response) {
   console.log(response.data);
   if (response.status == 200) {
@@ -18,3 +43,6 @@ let apiKey = "c461f833528021c73b417ab40d24a6b8";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Paris&units=metric&appid=${apiKey}`;
 
 axios.get(apiUrl).then(showTemperature);
+let dateElement = document.querySelector("#date");
+let currentTime = new Date();
+dateElement.innerHTML = formatDate(currentTime);
