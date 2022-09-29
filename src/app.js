64b45cd00d4e109afbe719b5d5a5ae016321e527
Mila Filinkova <109034507+MilaFilinkova@users.noreply.github.com>
2022-09-29 19:35:04 +0200

    Show the current temperature

--- src/app.js
+++ src/app.js
@@ -1,1 +1,20 @@
-alert("Hello World");
+function showTemperature(response) {
+  console.log(response.data);
+  if (response.status == 200) {
+    document.querySelector("#city").innerHTML = response.data.name;
+    document.querySelector("#temperature").innerHTML = Math.round(
+      response.data.main.temp
+    );
+    document.querySelector("#description").innerHTML =
+      response.data.weather[0].description;
+    document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+    document.querySelector("#wind").innerHTML = Math.round(
+      response.data.wind.speed
+    );
+  }
+}
+
+let apiKey = "c461f833528021c73b417ab40d24a6b8";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Paris&units=metric&appid=${apiKey}`;
+
+axios.get(apiUrl).then(showTemperature);
